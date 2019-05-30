390b40133d65b5a19a89dfe4ef7bb01169c5e4ee
Salvador Guzman <guzmansalv@gmail.com>
2019-05-29 22:23:28 -0700

    draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate reqwest;
+extern crate scraper;
+
 fn main() {
-    println!("Hello, world!");
+    let urls: Vec<String> = std::env::args().skip(1).collect();
+    let mut paths: std::collections::HashSet<&str> =
+        std::collections::HashSet::new();
+
+    for url in urls {
+        println!("Calling {}", url);
+        let url: &str = url.as_str();
+        let document: String = reqwest::get(url)
+            .expect("Request failed")
+            .text()
+            .expect("Body failed");
+        let document: &str = document.as_str();
+
+        let doc: scraper::Html = scraper::Html::parse_document(document);
+        let select: scraper::Selector =
+            scraper::Selector::parse("a[href]")
+                .expect("Failed to parse links");
+
+        for element in doc.select(&select) {
+            let href: &str =
+                element.value().attr("href").expect("Could not find attr{href}");
+
+            let pat: &'static str = "/";
+            if !href.starts_with(pat) {
+                continue;
+            }
+        }
+    }
+
+    for s in paths {
+        println!("{}", s);
+    }
+
+    println!("# of paths {}", paths.len());
 }
